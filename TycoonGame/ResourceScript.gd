2a96afacdc64e32fa59aa3257e7e2fa9026ff755
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-10-16 14:42:46 +0200

    Savefile aangepast
    alle resources worden nu goed opgehaald en opgeslagen

--- TycoonGame/ResourceScript.gd
+++ TycoonGame/ResourceScript.gd
@@ -9,15 +9,19 @@ var GFTPerSecond = 2
 var PaperPerSecond = 1
 
 func _ready():
-	
+	Plastic = SaveScript.LoadFile("main", "Plastic", 0)
+	GFT = SaveScript.LoadFile("main", "GFT", 0)
+	Paper = SaveScript.LoadFile("main", "Paper", 0)
 	$Plasticlabel.text = str(Plastic)
 	$GFTLabel.text = str(GFT)
 	$Paperlabel.text = str(Paper)
 	
+
+	
 func _on_House1_pressed():
 	Plastic += 1
 	$Plasticlabel.text = str(Plastic)
-
+	
 
 
 func _on_PlasticTimer_timeout():
@@ -33,3 +37,9 @@ func _on_GFTTimer_timeout():
 func _on_PaperTimer_timeout():
 	Paper += PaperPerSecond
 	$Paperlabel.text = str(Paper)
+
+
+func _on_SaveFile_timeout():
+	SaveScript.SaveFile("main", "Plastic", Plastic)
+	SaveScript.SaveFile("main", "GFT", GFT)
+	SaveScript.SaveFile("main", "Paper", Paper)
---
+++ TycoonGame/SaveScript.gd
@@ -0,0 +1,19 @@
+extends Node
+# voor het testen op mobiel moet res naar user worden veranderd
+var savePath = "res://save-file.cfg"
+var config = ConfigFile.new()
+var loadResponse = config.load(savePath)
+
+#section = voor welk gedeelte ermoet worden opgeslagen bv speler gegevens
+#key = de naam van wat er meot worden opgeslagen
+#value = de waarde die moet worden opgeslagen
+
+func SaveFile(section, key, value):
+	config.set_value(section, key, value)
+	config.save(savePath)
+	
+	
+func LoadFile(section, key, default):
+	return config.get_value(section, key , default)
+	
+	
