72b58cbf15fc64102b92bf377f3cae8f01e5fc31
luke-vg <vangenechtenluke@gmail.com>
2020-11-06 10:28:34 +0100

    Hello world script
    Made a hello world script that prints the text when a button is pressed

--- TycoonGame/QuestButton.gd
+++ TycoonGame/QuestButton.gd
@@ -1,4 +1,4 @@
-extends Control
+extends TextureButton
 
 
 # Declare member variables here. Examples:
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_QuestButton_pressed():
+	print ("test")
