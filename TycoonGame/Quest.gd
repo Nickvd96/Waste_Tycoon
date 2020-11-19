ab263c2cc4f73b44a45fed8c9816bea5b836b8bd
Robin Hein Anna Matheus Friesen III <44802556+robijntje@users.noreply.github.com>
2020-11-19 12:06:09 +0100

    Update Quest.gd

--- TycoonGame/Quest.gd
+++ TycoonGame/Quest.gd
@@ -4,10 +4,14 @@ onready var available_quests = $Available
 onready var active_quests = $Active
 onready var completed_quests = $Completed
 onready var delivered_quests = $Delivered
+var party : Party
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+func initialize(game, _party : Party ) -> void:
+	game.connect("combat_started", self, "_on_Game_combat_started")
+	party = _party
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
