7c517d5ba78b0fd076f6275e33fa4b2bb20b4ca6
Robin Hein Anna Matheus Friesen III <44802556+robijntje@users.noreply.github.com>
2020-11-19 14:53:31 +0100

    Connected script to CanObject
    Print 'Blikje' in console to confirm connection

---
+++ TycoonGame/QuestPlayScript.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var questScore = 4
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+func _on_CanObject_pressed():
+	#questScore = questScore -1
+	#CanObject.queue_free()
+	print("Blikje")
+	return
