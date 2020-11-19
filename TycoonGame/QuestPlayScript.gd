901d94c6393baf9791733db94c815426333100f3
Robin Hein Anna Matheus Friesen III <44802556+robijntje@users.noreply.github.com>
2020-11-19 15:06:12 +0100

    Added questScore update for all objects in quest
    Score will go one down every time an object gets pressed

--- TycoonGame/QuestPlayScript.gd
+++ TycoonGame/QuestPlayScript.gd
@@ -13,3 +13,21 @@ func _on_CanObject_pressed():
 	#CanObject.queue_free()
 	print("Blikje")
 	return
+
+func _on_BottleObject_pressed():
+	questScore = questScore -1
+	print(questScore)
+	print("Glas")
+	return
+	
+func _on_PaperObject_pressed():
+	questScore = questScore -1
+	print(questScore)
+	print("Papier")
+	return
+	
+func _on_MinecraftVlees_pressed():
+	questScore = questScore -1
+	print(questScore)
+	print("Minecraft vlees")
+	return
