be62c80a492cf979d0a76ee5a0f25a785c9204e8
luke-vg <vangenechtenluke@gmail.com>
2020-12-04 15:17:30 +0100

    Hotfix
    A laptop won't load because of an error. Commented them out because they were not necessary

--- TycoonGame/Quest.gd
+++ TycoonGame/Quest.gd
@@ -4,22 +4,22 @@ onready var available_quests = $Available
 onready var active_quests = $Active
 onready var completed_quests = $Completed
 onready var delivered_quests = $Delivered
-var party : Party
+#var party : Party
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-func initialize(game, _party : Party ) -> void:
-	game.connect("combat_started", self, "_on_Game_combat_started")
-	party = _party
+#func initialize(game, _party : Party ) -> void:
+	#game.connect("combat_started", self, "_on_Game_combat_started")
+	#party = _party
 	
-	func find_available(reference : Quest) -> Quest:
-		return available_quests.find(reference)
+	#func find_available(reference : Quest) -> Quest:
+		#return available_quests.find(reference)
 
 func get_available_quests() -> Array:
 	return available_quests.get_quests()
 
-func is_available(reference : Quest) -> bool:
-	return available_quests.find(reference) != null
+#func is_available(reference : Quest) -> bool:
+	#return available_quests.find(reference) != null
 	
 	
