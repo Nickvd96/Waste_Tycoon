e2e492251ad068392ffd169a39af3357ce8771e5
luke-vg <vangenechtenluke@gmail.com>
2021-01-08 11:24:35 +0100

    Correctly displaying numbers
    The popup message now displays a 1. Fixed it from before where number would clip through eachother

--- TycoonGame/PopupClickMessage.gd
+++ TycoonGame/PopupClickMessage.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 func _ready():
-	$PlasticPopUpLabel.text = "+" + str(GlobalScript.PlasticPerSecond)
-	$GFTPopUpLabel.text = "+" + str(GlobalScript.GFTPerSecond)
-	$PaperPopUpLabel.text = "+" + str(GlobalScript.PaperPerSecond)
-
+	$PlasticPopUpLabel.text = "+1"
+	#$GFTPopUpLabel.text = "+" + str(GlobalScript.GFTPerSecond)
+	#$PaperPopUpLabel.text = "+" + str(GlobalScript.PaperPerSecond)
+	
+	# Momenteel is de popup automatisch een 1. Er moeten aparte variabelen worden aangemaakt om ze dynamisch te
+	# updaten. Dit gebeurt in een later stadium
 func _physics_process(delta):
 	position.y -= 4
 	
